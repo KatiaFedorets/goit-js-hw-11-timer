82eedd767e7c7a6b0392254ca58eca26276d3c46
Katerina Fedorets <katia.fedorets@gmail.com>
2020-11-15 14:39:40 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,61 +4,6 @@ const hours = document.querySelector('.value[data-value="hours"]');
 const mins = document.querySelector('.value[data-value="mins"]');
 const secs = document.querySelector('.value[data-value="secs"]');
 
-// class CountdownTimer {
-//     constructor({ selector, targetDate }) {
-//         this.selector = selector;
-//         this.targetDate = targetDate;
-//         intervalId: null;
-//     }
-
-//     start() {
-//         const startDate = new Date(this.targetDate);
-
-//         this.intervalId = setInterval(() => {
-//             const currentTime = Date.now();
-//             const deltaTime = startDate - currentTime;
-//             const time = getTimeComponents(deltaTime);
-
-//             days.textContent = `${time.days}`;
-//             hours.textContent = `${time.hours}`;
-//             mins.textContent = `${time.mins}`;
-//             secs.textContent = `${time.secs}`;
-//         }, 1000);
-//     }
-
-//     stop() {
-//         if (deltaTim < 0) {
-//             clearInterval(this.intervalId);
-//         }
-//     }
-
-
-
-// }
-
-
-// const timer = new CountdownTimer({
-//     selector: "#timer-1",
-//     targetDate: new Date("May 15, 2021"),
-// });
-
-// timer.start();
-
-// function pad(value) {
-//     return String(value).padStart(2, "0");
-// };
-
-// function getTimeComponents(time) {
-//     const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-//     const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-//     const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-//     const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-//     return { days, hours, mins, secs };
-// };
-
-
-
-
 class CountdownTimer {
     constructor({ selector, targetDate, onTick }) {
         this.selector = selector;
@@ -80,11 +25,6 @@ class CountdownTimer {
             const deltaTime = startDate - currentTime;
             const time = this.getTimeComponents(deltaTime);
             this.onTick(time);
-
-            // days.textContent = `${time.days}`;
-            // hours.textContent = `${time.hours}`;
-            // mins.textContent = `${time.mins}`;
-            // secs.textContent = `${time.secs}`;
         }, 1000);
     }
 
